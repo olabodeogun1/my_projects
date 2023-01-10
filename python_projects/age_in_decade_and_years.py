cb9e0ece251c19a4c3f61fea01640b301c2c2675
Olabode Ogun <olabodeogun1@gmail.com>
2023-01-10 05:45:37 -0800

    decade and age converter

--- python_projects/age_in_decade_and_years.py
+++ python_projects/age_in_decade_and_years.py
@@ -1,4 +1,4 @@
 decade = int(input ("How old are you?\n"))
 decade2 = decade/10
 years = decade%10
-print("You are " + str(int(decade2)) + " decades old and " + str(years) + " old.")
+print("You are " + str(int(decade2)) + " decades and " + str(years) + " years old.")
