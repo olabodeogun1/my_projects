85050ca550ce6ad6dce461ee45e65ebb1d0cff15
Olabode Ogun <olabodeogun1@gmail.com>
2023-01-12 08:00:06 -0800

    Importing data using API

---
+++ python_projects/people_in_space.py
@@ -0,0 +1,11 @@
+import requests
+
+response = requests.get('http://api.open-notify.org/astros.json')
+
+json = response.json()
+
+print("The People in Space are:")
+
+for person in json['people']:
+
+        print(person['name'])
